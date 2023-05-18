9d26e2cd1f29d5493b35db574f214c99b78e8cb5
Michal Dariusz Nowakowski <michaldariusznowakowski@protonmail.com>
2023-05-18 20:50:53 +0200

    Lab 6 iris + wine

--- lab6/src/bayes.py
+++ lab6/src/bayes.py
@@ -5,82 +5,143 @@ import numpy as np
 from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, ClassifierMixin
+import matplotlib.pyplot as plt
+
+## Config
+
+LICZBA_PRZEDZIALOW = 20
+
+DEBUG = False
+
+####################
+
+SELFPATH = __file__[:__file__.rfind('\\') + 1]
+
+DATA = np.genfromtxt(SELFPATH+'wine.data', delimiter=',')
+
+LABELS = ['Class', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols',
+            'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+            'OD280/OD315 of diluted wines', 'Proline']
+CLASS = DATA[:, 0]
+DATA = DATA[:, 1:]
 
 
-# 1. Wczytaj dane z pobranego pliku tekstowego wine.data do macierzy numpy (wykorzystaj funkcję numpy.genfromtxt)
-#  i rozdziel tę macierz na dwie macierze X (o wymiarze
-# 178 × 13) i y (178 × 1 — etykiety klas).
-DATA = np.genfromtxt('wine.data', delimiter=',')
-# 2. Dyskretyzację danych „wine” można wykonać wykorzystując gotowy obiekt
-# KBinsDiscretizer (z pakietu sklearn.preprocessing) lub samodzielnie na poziomie opracowywanej 
-# klasy NBC (liczba przedziałów, na którą dyskretyzujemy cechy
-# oryginalnie ciągłe, powinna być parametrem nastawialnym przez użytkownika).
-
-DISCRETIZER = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
-
-# 3. Podziel dane na część uczącą i testową (wykorzystaj funkcję train_test_split z pakietu sklearn.model_selection).
-LEARN_DATA, TEST_DATA = train_test_split(DATA, test_size=0.3, random_state=42)
-
-# 4. Napisz klasę reprezentującą naiwny klasyfikator Bayesa w wariancie ze zmiennymi dyskretnymi. 
-# Klasę przygotuj zgodnie z ideą biblioteki scikit-learn — m.in.: wykonaj
-# dziedziczenie po klasach BaseEstimator i ClassifierMixin, przygotuj metody fit
-# (uczenie) i predict (klasyfikowanie - wskazanie identyfikatora klasy)
-# oraz pomocniczo predict_proba (podanie wartości obliczonego prawdopodobieństwa). (Szczegóły
-# w kolejnych podrozdziałach).
-
-class NBC(BaseEstimator, ClassifierMixin):
-    """Naiwny klasyfikator Bayesa w wariancie ze zmiennymi dyskretnymi."""
-
-    def __init__(self, alpha=1):
-        self.alpha = alpha
+class Bayes(BaseEstimator, ClassifierMixin):
+    def __init__(self):
+        self.prior = None
+        self.likelihood = None
+        self.classes = None
 
     def fit(self, X, y):
-        """Uczenie."""
-        self._classes = np.unique(y)
-        self._classes_count = self._classes.size
-        self._features_count = X.shape[1]
-        self._features_values = [np.unique(X[:, i]) for i in range(self._features_count)]
-        self._classes_prob = np.array([np.sum(y == c) / y.size for c in self._classes])
-        self._features_prob = np.array([
-            np.array([
-                np.sum((X[:, i] == v) & (y == c)) / np.sum(y == c) for v in self._features_values[i]
-            ]) for i in range(self._features_count)
-        ])
-        return self
+        self.classes = np.unique(y)
+        self.prior = np.zeros(len(self.classes))
+        self.likelihood = np.zeros((len(self.classes), X.shape[1], LICZBA_PRZEDZIALOW))
+        for i, c in enumerate(self.classes):
+            self.prior[i] = np.sum(y == c) / len(y)
+            for j in range(X.shape[1]):
+                for k in range(LICZBA_PRZEDZIALOW):
+                    self.likelihood[i, j, k] = np.sum((X[:, j] == k) & (y == c)) / np.sum(y == c)
 
     def predict(self, X):
-        """Klasyfikowanie."""
-        return np.array([self._classes[np.argmax(self.predict_proba(X)[i, :])] for i in range(X.shape[0])])
+        y_pred = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            p = np.zeros(len(self.classes))
+            for j, c in enumerate(self.classes):
+                p[j] = self.prior[j]
+                for k in range(X.shape[1]):
+                    p[j] *= self.likelihood[j, k, int(X[i, k])]
+            y_pred[i] = self.classes[np.argmax(p)]
+        return y_pred
 
-    def predict_proba(self, X):
-        """Prawdopodobieństwo."""
-        return np.array([
-            np.array([
-                self._classes_prob[c] * np.prod(self._features_prob[i, :, c][self._features_values[i] == X[j, i]])
-                for c in range(self._classes_count)
-            ]) for i in range(self._features_count)
-        ])
+    def clean(self):
+        self.prior = None
+        self.likelihood = None
+        self.classes = None
 
-# 5. Zastanów się i zaplanuj wg własnego uznania wygodne struktury danych do przechowywania:
-# • rozkładu a priori klas P(Y = y),
-# • rozkładów warunkowych P(Xj = v|Y = y).
-# Mogą to być tablice, słowniki, listy lub odpowiednie połączenia / zagnieżdżenia tych
-# struktur. Do tego celu potrzebne będzie także ustalenie dyskretnych dziedzin zmiennych,
-#  tj. wykrycie, jakie unikalne wartości poszczególne zmienne mogą przyjmować,
-# np. z wykorzystaniem funkcji numpy.unique. Przemyśl, czy informacje o dziedzinach
-# należy zdobywać na poziomie funkcji fit na podstawie danych uczących, czy też lepiej
-# przekazać je klasyfikatorowi już podczas konstrukcji.
+    def laplace(self, X, y):
+        self.classes = np.unique(y)
+        self.prior = np.zeros(len(self.classes))
+        self.likelihood = np.zeros((len(self.classes), X.shape[1], LICZBA_PRZEDZIALOW))
+        for i, c in enumerate(self.classes):
+            self.prior[i] = np.sum(y == c) / len(y)
+            for j in range(X.shape[1]):
+                for k in range(LICZBA_PRZEDZIALOW):
+                    self.likelihood[i, j, k] = (np.sum((X[:, j] == k) & (y == c)) + 1) / (np.sum(y == c) + LICZBA_PRZEDZIALOW)
 
-# 6. Wyznacz dokładność (Accuracy =
-#NP LZ ∗ 100%, gdzie NP — liczba próbek poprawnie
-# sklasyfikowanych w zadanym zbiorze, LZ — liczność zbioru) otrzymanego klasyfikatora
-# na zbiorach uczącym i testowym.
+    def accuracy(self, y_true, y_pred):
+        acc = np.sum(y_true == y_pred) / len(y_true) * 100
+        return round(acc, 2)
+    
 
-def accuracy(y_true, y_pred):
-    """Dokładność."""
-    return np.sum(y_true == y_pred) / y_true.size
+if DEBUG:
+    # plot data
+    plt.figure(figsize=(10, 10))
+    for i in range(1, 14):
+        plt.subplot(5, 3, i)
+        plt.scatter(DATA[:, i - 1], CLASS, s=1)
+        plt.title(LABELS[i])
+    plt.tight_layout()
+    plt.show()
 
-# 7. Obliczenia powtórz uwzględniając poprawkę LaPlace’a (możesz do tego celu 
-# wprowadzić przełącznik w konstruktorze Twojej klasy). Powtórz obliczenia - uczenie i pomiary
-# dokładności. Zwróć uwagę, czy poprawka LaPlace’a podnosi dokładność testową dla
-# tego zbioru danych.
+# deskrtyzacja
+est = KBinsDiscretizer(n_bins=LICZBA_PRZEDZIALOW, encode='ordinal', strategy='uniform')
+DATA_D = est.fit_transform(DATA) 
+# plot data
+if DEBUG:
+    plt.figure(figsize=(10, 10))
+    for i in range(1, 14):
+        plt.subplot(5, 3, i)
+        plt.scatter(DATA_D[:, i - 1], CLASS, s=1)
+        plt.title(LABELS[i])
+    plt.tight_layout()
+    plt.show()
+
+# podział na część uczącą i testową
+X_train, X_test, y_train, y_test = train_test_split(DATA_D, CLASS, test_size=0.3, random_state=42)
+
+## Bayes 
+bayes = Bayes()
+bayes.fit(X_train, y_train)
+y_pred = bayes.predict(X_test)
+print('Bayes accuracy: ', bayes.accuracy(y_test, y_pred) , '%')
+
+## Bayes z wykorzystaniem estymatora Laplace'a
+bayes.clean()
+bayes.laplace(X_train, y_train)
+y_pred = bayes.predict(X_test)
+print('Bayes with Laplace accuracy: ', bayes.accuracy(y_test, y_pred), '%')
+
+
+
+##################
+## Test na danych z iris.data
+DATA = np.genfromtxt(SELFPATH+'iris.data', delimiter=',', dtype=str)
+CLASS = DATA[:, 4]
+# zmiana etykiet na liczby
+CLASS[CLASS == 'Iris-setosa'] = 0
+CLASS[CLASS == 'Iris-versicolor'] = 1
+CLASS[CLASS == 'Iris-virginica'] = 2
+CLASS = CLASS.astype(int)
+
+DATA = DATA[:, 1:]
+DATA = DATA.astype(float)
+
+# deskrtyzacja
+est = KBinsDiscretizer(n_bins=LICZBA_PRZEDZIALOW, encode='ordinal', strategy='uniform')
+DATA_D = est.fit_transform(DATA)
+
+# podział na część uczącą i testową
+X_train, X_test, y_train, y_test = train_test_split(DATA_D, CLASS, test_size=0.3, random_state=42)
+
+## Bayes
+bayes.clean()
+bayes.fit(X_train, y_train)
+y_pred = bayes.predict(X_test)
+print('Bayes accuracy: ', bayes.accuracy(y_test, y_pred) , '%')
+
+## Bayes z wykorzystaniem estymatora Laplace'a
+bayes.clean()
+bayes.laplace(X_train, y_train)
+y_pred = bayes.predict(X_test)
+print('Bayes with Laplace accuracy: ', bayes.accuracy(y_test, y_pred) , '%')
+
